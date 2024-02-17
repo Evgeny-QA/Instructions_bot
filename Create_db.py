fc29185bb6dbd54dc32694c4b3ac681afa3b7463
Evgen-QA <evgenygmgqa@gmail.com>
2024-02-17 14:40:35 +0300

    added user lvls (table Admins), user_ids for vk and tg (table Users)

--- Create_db.py
+++ Create_db.py
@@ -4,20 +4,28 @@ import sqlite3 as sql
 with sql.connect('Instructions_bot_database.db') as db:
     cursor = db.cursor()
 
+    '''Создание основной базы данных и заполнение таблицы Admins с уровнями доступов'''
     cursor.executescript("""
     CREATE TABLE IF NOT EXISTS Admins(
         id INTEGER PRIMARY KEY,
         access INTEGER DEFAULT 0,
         name_access TEXT
     );
+        
+    INSERT INTO Admins(access, name_access)
+    VALUES (0, "Пользователь"),
+           (1, "Админ"),
+           (2, "Системный админ");
 
     CREATE TABLE IF NOT EXISTS Users(
         id INTEGER PRIMARY KEY,
-        user_status INTEGER,
+        user_status INTEGER DEFAULT 1,
         user_name TEXT,
         phone_number TEXT UNIQUE,
         password TEXT,
         configuration_pc TEXT,
+        telegram_id TEXT,
+        vk_id TEXT,
         FOREIGN KEY (user_status) REFERENCES Admins(id)
     );
 
