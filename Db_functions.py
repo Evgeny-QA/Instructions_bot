0c6ccb078785b05c557d17664ffed522214d268f
Evgen-QA <evgenygmgqa@gmail.com>
2024-02-17 20:10:33 +0300

    added func get_telegram_id_authorized_users for local vars, added func get_all_sysadmins_and_admins_phones for get_start_bot_info_from_json func

--- Db_functions.py
+++ Db_functions.py
@@ -13,6 +13,25 @@ class DataBase:
         with open("log_changes.txt", "a") as log:
             log.write(str(datetime.now())[:19] + "\n" + text + "\n")
 
+    def get_telegram_id_authorized_users(self):
+        """Получение авторизованных пользователей при перезапуске бота
+        :return: словарь с ключами id пользователей"""
+        try:
+            with self.db:
+                cursor = self.db.cursor()
+                cursor.execute('''SELECT telegram_id
+                                  FROM Users''')
+                ids = [id[0] for id in cursor.fetchall()]
+                users_telegram_id = dict()
+
+                for id in ids:
+                    if id is not None:
+                        users_telegram_id[id[0]] = ["", "", ""]
+                return users_telegram_id
+        except sql.Error as error:
+            print(f"Произошла ошибка: {error}")
+            return False
+
     def get_start_bot_info_from_json(self):
         """Получение для запуска бота и вставка админов в бд, если их нет
         :return: Возвращает TOKEN для подключения к боту"""
@@ -20,16 +39,10 @@ class DataBase:
             with open('JSON_CONFIG.json', 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 TOKEN, main_admins, admins = data["TOKEN"], data["main_admins"], data["admins"]
-
             # print(TOKEN, main_admins, admins, sep="\n")
             with self.db:
                 cursor = self.db.cursor()
-                '''Проверка на наличие админов в бд'''
-                cursor.execute('''SELECT phone_number
-                                  FROM Users U JOIN Admins A ON U.user_status == A.id
-                                  WHERE access IN (2, 3)''')
-                registered_phones = [phone[0] for phone in cursor.fetchall()]
-                # print(registered_phones)
+                registered_phones = DataBase().get_all_sysadmins_and_admins_phones()
 
                 '''Добавление системных админов и админов'''
                 for phone_user in main_admins:
@@ -46,3 +59,21 @@ class DataBase:
         except sql.Error as error:
             print(f"Произошла ошибка: {error}")
             return False
+
+    def get_all_sysadmins_and_admins_phones(self):
+        """Получение всех номеров зарегистрированных админов и системных админов
+        :return: Возвращает список номеров"""
+        try:
+            with self.db:
+                cursor = self.db.cursor()
+                cursor.execute('''SELECT phone_number
+                                  FROM Users U JOIN Admins A ON U.user_status == A.id
+                                  WHERE access IN (2, 3)''')
+                return [phone[0] for phone in cursor.fetchall()]
+        except sql.Error as error:
+            print(f"Произошла ошибка: {error}")
+            return False
+
+
+# DataBase().get_start_bot_info_from_json()
+# DataBase().get_telegram_id_authorized_users()
