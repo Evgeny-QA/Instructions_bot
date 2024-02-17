f8117ff4484311cc20e195b19b9ef62d1ebd46b2
Evgen-QA <evgenygmgqa@gmail.com>
2024-02-17 20:12:15 +0300

    import funcs for Start_bot.py and settings

--- Start_bot.py
+++ Start_bot.py
@@ -1,8 +1,17 @@
-import json
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Db_functions import DataBase
 
 
-'''Получение токена для старта бота и админов для управления ботов'''
-with open('JSON_CONFIG.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
-    TOKEN = data["TOKEN"]
-    print(TOKEN)
+DB = DataBase()
+'''Запуск бота, получение первоначальных данных для работы с ботом'''
+bot = telebot.TeleBot(DB.get_start_bot_info_from_json())
+dict_users_id_info = DB.get_telegram_id_authorized_users()
+print(dict_users_id_info)
+
+
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text in ('/start', '/menu'):
+        if message.from_user.id not in dict_users_id_info:
+            bot.send_message(message.chat.id, "Пожалуйста, войдите или зарегистрируйтесь")
