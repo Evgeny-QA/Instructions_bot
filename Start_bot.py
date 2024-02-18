b8753245ca9f2c30161162c4d1c401c2867ec0b8
Evgen-QA <evgenygmgqa@gmail.com>
2024-02-18 19:45:17 +0300

    changes in registration

--- Start_bot.py
+++ Start_bot.py
@@ -6,9 +6,9 @@ from Db_functions import DataBase
 DB = DataBase()
 '''Запуск бота, получение первоначальных данных для работы с ботом'''
 bot = telebot.TeleBot(DB.get_start_bot_info_from_json())
-dict_users_id_info = DB.get_telegram_id_authorized_users()
-dict_first_reg_id = dict()  # Информация для регистрации: [шаг регистрации, id пользователя, (1)имя и фамилия,
-                            # (2)номер телефона, (3)пароль, (4)конфигурация пк]
+dict_users_id_info = DB.get_all_telegram_id_authorized_users()  # {user_id: [access, "", ""]
+dict_first_reg_or_log_id = dict()  # Информация для регистрации: [шаг регистрации, id пользователя, (1)имя и фамилия,
+                                   # (2)номер телефона, (3)пароль, (4)конфигурация пк] / вход [номер (10), пароль (11)]
 print(dict_users_id_info)
 
 
@@ -20,6 +20,8 @@ kw_reg_or_log.add(InlineKeyboardButton("Войти", callback_data="log_in"),
 @bot.message_handler(content_types=['text'])
 def start(message):
     user_id = message.from_user.id
+    if user_id not in dict_first_reg_or_log_id:
+        dict_first_reg_or_log_id[user_id] = [""]
 
     '''Запрос на авторизацию/регистрацию'''
     if message.text == "/start":
@@ -27,38 +29,36 @@ def start(message):
             bot.send_message(message.chat.id, "Пожалуйста, войдите или зарегистрируйтесь", reply_markup=kw_reg_or_log)
 
     '''Регистрация'''
-    if dict_first_reg_id[user_id][0] == 1:
+    if dict_first_reg_or_log_id[user_id][0] == 1:
         '''Валидация длинны Имени и Фамилии'''
         if len(message.text) <= 40:
-            dict_first_reg_id[user_id][0] = 2
-            dict_first_reg_id[user_id].append(message.text)  # Имя и Фамилия
+            dict_first_reg_or_log_id[user_id][0] = 2
+            dict_first_reg_or_log_id[user_id].append(message.text)  # Имя и Фамилия
             bot.send_message(message.chat.id, "Введите номер телефона (с кодом +375 (+11 символов) или 80 (+11 символов)):")
         else:
             bot.send_message(message.chat.id, "Веденные Имя и Фаилия превышают допустимую длину в 40 символов, "
                                               "повторите ввод:")
-    elif dict_first_reg_id[user_id][0] == 2:
+    elif dict_first_reg_or_log_id[user_id][0] == 2:
         tel = message.text.strip()
         scheme = "('{0}'[:4] == '+375' and '{0}'[1:].isdigit() and len('{0}') == 13) or" \
                  "('{0}'[:2] == '80' and '{0}'[1:].isdigit() and len('{0}') == 11)"
         '''Валидация введенного номера телефона'''
         if eval(scheme.format(tel)):
-            dict_first_reg_id[user_id][0] = 3
-            dict_first_reg_id[user_id].append(message.text)  # Телефон
+            dict_first_reg_or_log_id[user_id][0] = 3
+            dict_first_reg_or_log_id[user_id].append(message.text)  # Телефон
             bot.send_message(message.chat.id, "Введите пароль:")
         else:
             bot.send_message(message.chat.id, "Неверный номера телефона, повторите попытку:")
-    elif dict_first_reg_id[user_id][0] == 3:
-        dict_first_reg_id[user_id][0] = 4
-        dict_first_reg_id[user_id].append(message.text)  # Пароль
+    elif dict_first_reg_or_log_id[user_id][0] == 3:
+        dict_first_reg_or_log_id[user_id][0] = 4
+        dict_first_reg_or_log_id[user_id].append(message.text)  # Пароль
         bot.send_message(message.chat.id, "Введите вашу конфигурацию компьютера:")
-    elif dict_first_reg_id[user_id][0] == 4:
-        dict_first_reg_id[user_id][0] = [""]
-        dict_first_reg_id[user_id].append(message.text)  # Характеристики ПК
-        DB.write_down_actions_to_log_file(f"Пользователь {dict_first_reg_id[user_id][2]} - "
-                                          f"({dict_first_reg_id[user_id][3]}) был зарегистрирован")
-        dict_users_id_info[dict_first_reg_id[user_id][1]] = ["", "", ""]
+    elif dict_first_reg_or_log_id[user_id][0] == 4:
+        dict_first_reg_or_log_id[user_id][0] = [""]
+        dict_first_reg_or_log_id[user_id].append(message.text)  # Характеристики ПК
+        DB.register_new_user(dict_first_reg_or_log_id[user_id][1:])
+        dict_users_id_info[dict_first_reg_or_log_id[user_id][1]] = [1, "", ""]
         bot.send_message(message.chat.id, "Вы успешно зарегистрировались!")
-        print(dict_first_reg_id[user_id])
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -67,9 +67,13 @@ def query_handler(call):
     chat_id = call.message.chat.id
     user_id = call.from_user.id
 
-    '''Отправка сообщения для регистрации'''
+    '''Отправка сообщения для входа и регистрации'''
     if call.data == "registration":
-        dict_first_reg_id[user_id] = [1, user_id]
+        dict_first_reg_or_log_id[user_id] = [1, user_id]
         bot.send_message(chat_id, "Введите Имя и Фамилию (не более 40 символов):")
     elif call.data == "log_in":
-        bot.send_message(chat_id, "Войти")
+        dict_first_reg_or_log_id[user_id] = [10, 11]
+        bot.send_message(chat_id, "Введите номер телефона:")
+
+print("Ready")
+bot.infinity_polling()
